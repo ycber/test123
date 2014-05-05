ac24676e09c2489cdd8b97792eb8f88e0dcffd00
Timm Stooss <github@gobon.de>
2014-05-05 20:25:30 +0200

    strings geändert

--- Serie2_3_Strings.c
+++ Serie2_3_Strings.c
@@ -21,7 +21,6 @@ typedef struct s_mystring {
 int string2mystring(char *s, mystring *ms) {
 
    int cnt = 0;
-
    while (s[cnt] != 0) {
       (*ms).content[cnt] = s[cnt];
       cnt++;
@@ -30,13 +29,7 @@ int string2mystring(char *s, mystring *ms) {
          return 1;
       }
    }
-
-printf("cnt: \"%d\"\n", cnt);
-
    (*ms).length = cnt;
-
-printf("(*ms).length: \"%d\"\n", (*ms).length);
-
    return 0;
 }
 
@@ -44,20 +37,13 @@ printf("(*ms).length: \"%d\"\n", (*ms).length);
 
 int mystring2string(mystring *ms, char *s, int size) {
 
-printf("(*ms).length: \"%d\"\n", (*ms).length);
-printf("size: \"%d\"\n", size);
    int cnt;
    int end;
    end = (*ms).length < size ? (*ms).length : size;
    for (cnt = 0; cnt < end; cnt++) {
       s[cnt] = (*ms).content[cnt];
    }
-
    s[cnt] = 0;
-
-printf("(*ms).length: \"%d\"\n", (*ms).length);
-printf("size: \"%d\"\n", size);
-
    if ((*ms).length > size ) {
       return 1;
    }
@@ -75,8 +61,6 @@ int main() {
    mystring x;
    mystring y;
 
-printf("x.length: \"%d\"\n", x.length);
-
 
    if (string2mystring(a,&x)) {
       printf("Die Konvertierung des folgenden Strings in die Struktur war nicht erfolgreich: \n\"%s\"  \n", a);
@@ -84,24 +68,7 @@ printf("x.length: \"%d\"\n", x.length);
    else {
       printf("Die Konvertierung des folgenden Strings in die Struktur war erfolgreich: \n\"%s\"  \n", a);
    }
-
-printf("x.length: \"%d\"\n", x.length);
-
-
    printf("\n");
-
-printf("x.length: \"%d\"\n", x.length);
-
-   if (string2mystring(b,&y)) {
-      printf("Die Konvertierung des folgenden Strings in die Struktur war nicht erfolgreich: \n\"%s\"  \n", b);
-   }
-   else {
-      printf("Die Konvertierung des folgenden Strings in die Struktur war erfolgreich: \n\"%s\"  \n", b);
-   }
-   printf("\n");
-
-
-printf("x.length: \"%d\"\n", x.length);
 
 
 
@@ -112,8 +79,6 @@ printf("x.length: \"%d\"\n", x.length);
    else {
       printf("Die Konvertierung der Struktur in String war erfolgreich: \n\"%s\"  \n", c);
    }
-
-
    printf("\n");
 
 
@@ -123,10 +88,16 @@ printf("x.length: \"%d\"\n", x.length);
    else {
       printf("Die Konvertierung der Struktur in String war erfolgreich: \n\"%s\"  \n",d);
    }
+   printf("\n");
 
-  printf("\n");
-   printf("c is: \"%s\"\n", c);
-   printf("d is: \"%s\"\n", d);
+   if (string2mystring(b,&y)) {
+       printf("Die Konvertierung des folgenden Strings in die Struktur war nicht erfolgreich: \n\"%s\"  \n", b);
+   }
+   else {
+      printf("Die Konvertierung des folgenden Strings in die Struktur war erfolgreich: \n\"%s\"  \n", b);
+   }
+   printf("\n");
+
 
 
    return 0;
